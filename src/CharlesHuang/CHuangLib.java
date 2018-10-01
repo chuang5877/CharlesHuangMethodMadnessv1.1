1ab8d1e4ac9a059c08739166d70b48ee1bd80738
chuang5877 <chuang5877@bths.edu>
2018-10-01 08:40:39 -0400

    fooBarBaz

--- src/CharlesHuang/CHuangLib.java
+++ src/CharlesHuang/CHuangLib.java
@@ -1,7 +1,19 @@
 package CharlesHuang;
 
 public class CHuangLib {
-    public static int leastCommonMultiple(int n1,int n2, int n3){
-        
+    public static void fooBarBaz(int n1){
+        String out="";
+        for(int n=1;n<=n1;n++) {
+            if (n % 15 == 0) {
+                out += "baz,";
+            } else if (n % 5 == 0) {
+                out += "bar,";
+            } else if (n % 3 == 0) {
+                out += "foo,";
+            } else {
+                out += n + ",";
+            }
+        }
+        System.out.println(out);
     }
 }
--- src/CharlesHuang/Main.java
+++ src/CharlesHuang/Main.java
@@ -3,6 +3,6 @@ package CharlesHuang;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        CHuangLib.fooBarBaz(15);
     }
 }
